3916e5fe51dfcc1c6b44b341776036c748695af5
Harry Markley <tremarkley@gmail.com>
2018-04-26 16:29:43 -0700

    Styled card header

--- src/app/components/card.jsx
+++ src/app/components/card.jsx
@@ -6,7 +6,11 @@ const Card = () => (
   <li className="card">
     <div className="color"></div>
     <div className="cardContent">
-      <div className="cardHeader"></div>
+      <div className="cardHeader">
+        <div className="cardHeaderText">
+          <h1>Grocery List</h1>
+        </div>
+      </div>
       <div className="cardBody"></div>
     </div>
   </li>
