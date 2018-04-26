4ddadc1994da449ffc8fb29872a9ff750639bc50
Harry Markley <tremarkley@gmail.com>
2018-04-26 15:25:19 -0700

    Created initial card component

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import resetStyle from '../../../css/reset.css';
 import style from '../../../css/app.css';
+import Card from './card';
 
 class App extends React.Component {
   constructor() {
@@ -13,13 +14,21 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <ul>
-          <li>
+        <ul className="navBar">
+          <li className="navItem">
             <button className="addNoteBtn"><div className="plusIcon"><span>+</span></div>Add Note</button>
           </li>
         </ul>
         <div className="content">
-
+          <ul className="flex-container">
+            {/* <li className="flex-item">1</li>
+            <li className="flex-item">2</li>
+            <li className="flex-item">3</li>
+            <li className="flex-item">4</li>
+            <li className="flex-item">5</li>
+            <li className="flex-item">6</li> */}
+            <Card />
+          </ul>
         </div>
       </div>
     );
---
+++ src/app/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from '../../../css/card.css';
+
+const Card = () => (
+  <li className="card">
+    <div className="color"></div>
+    <div className="cardContent">
+      <div className="cardHeader"></div>
+      <div className="cardBody"></div>
+    </div>
+  </li>
+)
+
+export default Card;
